06a53d32d1caf17e86e5da31fb212bace2346812
D179090 <102134977+D179090@users.noreply.github.com>
2025-12-03 11:55:24 +0300

    changes in func analyze sec and add func imports

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -13,25 +15,69 @@ func usage() {
 	fmt.Println("Example: analyzer C:\\Binaries\\notepad.exe")
 }
 
+func sectionName(sec peparser.Section) string {
+	nameBytes := sec.Header.Name[:]
+	n := bytes.IndexByte(nameBytes, 0)
+	if n == -1 {
+		n = len(nameBytes)
+	}
+	return string(nameBytes[:n])
+}
+
 // analyzePE открывает и парсит PE-файл и выводит простую информацию о секциях.
 func analyzePE(filename string) error {
-	pe, err := peparser.New(filename, &peparser.Options{})
+	f, err := peparser.New(filename, &peparser.Options{})
 	if err != nil {
 		return fmt.Errorf("error opening file: %w", err)
 	}
 
-	if err := pe.Parse(); err != nil {
+	if err := f.Parse(); err != nil {
 		return fmt.Errorf("error parsing PE: %w", err)
 	}
 
 	fmt.Printf("Successfully parsed %s\n", filename)
-	fmt.Printf("Number of sections: %d\n", len(pe.Sections))
+	fmt.Printf("Number of sections: %d\n", len(f.Sections))
 
-	for _, sec := range pe.Sections {
-		//fmt.Printf("Section Name : %s\n", sec.NameString())
-		fmt.Printf("Section VirtualSize : %x\n", sec.Header.VirtualSize)
-		fmt.Printf("Section Flags : %x, Meaning: %v\n\n",
-			sec.Header.Characteristics, sec.PrettySectionFlags())
+	for _, sec := range f.Sections {
+		name := sectionName(sec)
+		vsize := sec.Header.VirtualSize
+		char := sec.Header.Characteristics
+		entropy := sec.CalculateEntropy(f) // согласно документации
+
+		fmt.Printf("Section Name: %s\n", name)
+		fmt.Printf("  VirtualSize: 0x%x\n", vsize)
+		fmt.Printf("  Characteristics: 0x%x\n", char)
+		fmt.Printf("  Entropy: %.3f\n\n", entropy)
+	}
+
+	return nil
+}
+
+// importsPE выводит таблицу импортов.
+func importsPE(filename string) error {
+	f, err := peparser.New(filename, &peparser.Options{})
+	if err != nil {
+		return fmt.Errorf("error opening file: %w", err)
+	}
+
+	if err := f.Parse(); err != nil {
+		return fmt.Errorf("error parsing PE: %w", err)
+	}
+
+	if len(f.Imports) == 0 {
+		log.Printf("No imports found in %s\n", filename)
+		return nil
+	}
+
+	for _, imp := range f.Imports {
+		log.Printf("DLL: %s", imp.Name)
+		for _, e := range imp.Functions {
+			if e.Name != "" {
+				log.Printf("  -> %s", e.Name)
+			} else {
+				log.Printf("  -> ord: %d", e.Ordinal)
+			}
+		}
 	}
 
 	return nil
@@ -61,5 +107,12 @@ func main() {
 	if err := analyzePE(firstArgument); err != nil {
 		fmt.Printf("Analysis error: %v\n", err)
 		os.Exit(1)
+	}
+
+	if firstArgument == "imports" || firstArgument == "--imports" || firstArgument == "-i" {
+		if err := importsPE(firstArgument); err != nil {
+			fmt.Printf("Imports extraction error: %v\n", err)
+			os.Exit(1)
+		}
 	}
 }
